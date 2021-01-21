31e3e00be21a8751b845606979d643ab3a4610b1
psyche16 <you@example.com>
2021-01-21 20:40:00 +0530

    last Commit

---
+++ js/custom.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+   
+    for(var i=0;i<10;i++){
+        $('#animeVideoD').append(' <div class="section-title"><h5>Episode 1</h5></div><div class="anime__video__player"><video width="350" height="200" controls poster="img/hero/onepiece.png"><source src="videos/1.mp4" type="video/mp4">Your browser does not support the video tag.</video></div>');
+    }
+    //  $('#animeVideoD').append(' <div class="col-lg-4 col-md-4"><div class="section-title"><h5>Popular Videos</h5></div><div class="anime__video__player"><video width="350" height="200" controls poster="img/hero/onepiece.png"><source src="videos/1.mp4" type="video/mp4">Your browser does not support the video tag.</video></div></div>');
+    
+    //  $('#animeVideoD').append(' <div class="col-lg-4 col-md-4"><div class="section-title"><h5>Popular Videos</h5></div><div class="anime__video__player"><video width="350" height="200" controls poster="img/hero/onepiece.png"><source src="videos/1.mp4" type="video/mp4">Your browser does not support the video tag.</video></div></div>');
+    //  $('#animeVideoD').append(' <div class="col-lg-4 col-md-4"><div class="section-title"><h5>Popular Videos</h5></div><div class="anime__video__player"><video width="350" height="200" controls poster="img/hero/onepiece.png"><source src="videos/1.mp4" type="video/mp4">Your browser does not support the video tag.</video></div></div>');
+    //  $('#animeVideoD').append(' <div class="col-lg-4 col-md-4"><div class="section-title"><h5>Popular Videos</h5></div><div class="anime__video__player"><video width="350" height="200" controls poster="img/hero/onepiece.png"><source src="videos/1.mp4" type="video/mp4">Your browser does not support the video tag.</video></div></div>');
+    //  $('#animeVideoD').append(' <div class="col-lg-4 col-md-4"><div class="section-title"><h5>Popular Videos</h5></div><div class="anime__video__player"><video width="350" height="200" controls poster="img/hero/onepiece.png"><source src="videos/1.mp4" type="video/mp4">Your browser does not support the video tag.</video></div></div>');
+});
+
+
+// Onepunchman season 2 start //
+function opms20() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/41RYAY40xYuKarL/";
+  }
+
+  function opms21() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/xorYm2A8Z9Ukr9x/";
+  }
+
+  function opms22() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/1pY1pVa8QwCJRO/";
+  }
+
+  function opms23() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/KQ369mpgJxcbZY/";
+  }
+
+  function opms24() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/pLJo0OPdxzHr3rz/";
+  }
+
+  function opms25() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/9v4m71RV2Yca2xw/";
+  }
+
+  function opms26() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/69MkmZP8wlTZ04/";
+  }
+
+  function opms27() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/9W8889mB74fBQ4/";
+  }
+
+  function opms28() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/kqOPRpd6q7cYG2/";
+  }
+
+  function opms29() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/2LL1vzRVDyuZkdY/";
+  }
+
+  function opms210() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/86a8a9xlxYSX7d/";
+  }
+
+  function opms211() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/GebOaxkx34HaJz/";
+  }
+
+  function opms212() {
+    document.getElementById("myVideoTag").src = "https://streamtape.com/e/23M7pp8oqKtZMqO/";
+  }
+
+  //onepunchman season 2 end //
